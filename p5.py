173a67905ede60bed382b6d10badbb8c87b6f97a
abbiejones <abjones@pdx.edu>
2019-01-05 14:52:58 -0800

    completed problem 5

---
+++ p5.py
@@ -0,0 +1,29 @@
+# cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+#
+# Given this implementation of cons:
+#
+# def cons(a, b):
+#     def pair(f):
+#         return f(a, b)
+#     return pair
+# Implement car and cdr.
+
+
+def cons(a, b):
+     def pair(f):
+         return f(a, b)
+     return pair
+
+def car(func):
+    pair = func(lambda a,b : a)
+    return pair
+
+def csr(func):
+    pair = func(lambda a,b : b)
+    return pair
+
+def main():
+    print(car(cons(1,2)))
+    print(csr(cons(1,2)))
+
+main()
